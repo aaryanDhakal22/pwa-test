55c5fe73a539afc51b27ed88b468a1d457a66c7e
aaryanDhakal22 <aaryan.dhakal22@gmail.com>
2022-07-08 13:00:31 +0545

    final touches

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function rickit(){
+window.addEventListener("load", function () {
+    // do things after the DOM loads fully
+    btn = document.getElementById("roller")
+// When the user clicks the button, open the modal 
+btn.addEventListener('click', function () {
     document.getElementById("ricker").style.display = "block"
-}
+});
+  });
+    
